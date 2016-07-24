a066b18a32a29cc73885447af5773893fb92fc42
Thomas Kolasa <tkolasa2028@gmail.com>
2016-07-24 15:55:06 -0400

    Started competition 'Titanic: Machine Learning from Disaster'

---
+++ titanic_machine_learning_from_disaster/model1_all_died.py
@@ -0,0 +1,10 @@
+"""
+Creating my first Titanic submission. In this model, I predict everybody dies.
+    ___o .--.
+   /___| |OO|
+  /'   |_|  |_
+       (_    _)
+       | |   \
+       | |oo_/
+"""
+
