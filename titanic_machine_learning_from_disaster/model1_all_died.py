3312bdb14d185c9264802559c621d49b2a4ac61a
Thomas Kolasa <tkolasa2028@gmail.com>
2016-07-24 16:21:41 -0400

    Titanic: predicting all died.

--- titanic_machine_learning_from_disaster/model1_all_died.py
+++ titanic_machine_learning_from_disaster/model1_all_died.py
@@ -1,5 +1,5 @@
 """
-Creating my first Titanic submission. In this model, I predict everybody dies.
+Creating my first Titanic submission. In this model, I predict everybody dies. Turns out, 63% of people died.
     ___o .--.
    /___| |OO|
   /'   |_|  |_
@@ -8,3 +8,16 @@ Creating my first Titanic submission. In this model, I predict everybody dies.
        | |oo_/
 """
 
+import pandas as pd
+
+# Load the test data
+df = pd.read_csv('test.csv')
+
+# Remove all columns except for PassengerId
+df = df[['PassengerId']]
+
+# Predict that everybody dies
+df['Survived'] = 0
+
+# Save the file for submission
+df.to_csv('submissions/model1_all_died.csv', index=False)
